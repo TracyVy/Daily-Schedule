4be1fef2a52970c477d42621afb10185f6adbe88
Tracy Vy <tracyvy88@gmail.com>
2020-05-01 15:35:16 -0700

    add quote of the day

--- Develop/app.js
+++ Develop/app.js
@@ -4,9 +4,26 @@ console.log(currentDate);
 $("#currentDate").html(currentDate);
 
 // Allow user's input to persist, even after refresh
-var savedText = document.getElementById("#input");
-localStorage.setItem("savedText", input.val());
+// var savedText = document.getElementById("#input");
+// localStorage.setItem("savedText", savedText.value);
 
+$(document).ready(function () {
+  console.log("hello");
+  var ajaxConfig = {
+    url: "http://quotes.rest/qod.json",
+    type: "GET",
+    dataType: "JSON",
+  };
+
+  $.ajax(ajaxConfig).then(function (response) {
+    var output = response.contents.quotes[0];
+    console.log(output.author, output.quote);
+    //.html replaces previous html
+    $("#quoteOTD").html(`<p>${output.quote}  ${output.author}</p>`);
+  });
+});
+
+/*
 window.onload = function () {
   document.getElementById("#saveBtn").onclick = function (e) {
     e.preventDefault();
@@ -27,6 +44,7 @@ function get() {
   document.getElementById("openedText").innerHTML = storedITem + "OPENED";
 }
 
-// Color-code time block: past (grey), current (red), future (green)
+Color-code time block: past (grey), current (red), future (green)
 var currentTime = moment().format("LT");
 console.log(currentTime);
+*/
