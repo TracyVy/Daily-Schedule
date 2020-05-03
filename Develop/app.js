f5d7f04a55091e12ff1631be4468abc1b5b8c1c8
Tracy Vy <tracyvy88@gmail.com>
2020-05-03 15:56:40 -0700

    military time comparison

--- Develop/app.js
+++ Develop/app.js
@@ -1,9 +1,9 @@
 // Display current date
 var m = moment();
-var currentHour = m.format("h a");
+var currentHour = m.format("HH");
 var currentDate = m.format("MMMM Do, YYYY");
 console.log(currentHour);
-$("#currentDate").html(currentDate);
+$(".dateLabel").html(currentDate);
 
 // AJAX: quote of the day
 $(document).ready(function () {
@@ -21,16 +21,65 @@ $(document).ready(function () {
     $("#quoteOTD").html(`<p>${output.quote}  ${output.author}</p>`);
   });
 
-  // if currentHour >
+  // Set color-coded functions: past (grey), current (red), future (green)
+  if (currentHour > 9) {
+    $(".hour1").attr("style", "background-color: grey;");
+  } else if ((currentHour = 9)) {
+    console.log("XXX hour1=" + $(".hour1"));
+    $(".hour1").attr("style", "background-color: red;");
+  } else $(".hour1").attr("style", "background-color: green;");
+
+  if (currentHour > 10) {
+    $(".hour2").attr("style", "background-color: grey;");
+  } else if ((currentHour = 10)) {
+    $(".hour2").attr("style", "background-color: red;");
+  } else $(".hour2").attr("style", "background-color: green;");
+
+  if (currentHour > 11) {
+    $(".hour3").attr("style", "background-color: grey;");
+  } else if ((currentHour = 11)) {
+    $(".hour3").attr("style", "background-color: red;");
+  } else $(".hour3").attr("style", "background-color: green;");
+
+  if (currentHour > 12) {
+    $(".hour4").attr("style", "background-color: grey;");
+  } else if ((currentHour = 12)) {
+    $(".hour4").attr("style", "background-color: red;");
+  } else $(".hour4").attr("style", "background-color: green;");
+
+  if (currentHour > 13) {
+    $(".hour5").attr("style", "background-color: grey;");
+  } else if ((currentHour = 13)) {
+    $(".hour5").attr("style", "background-color: red;");
+  } else $(".hour5").attr("style", "background-color: green;");
+
+  if (currentHour > 14) {
+    $(".hour6").attr("style", "background-color: grey;");
+  } else if ((currentHour = 14)) {
+    $(".hour6").attr("style", "background-color: red;");
+  } else $(".hour6").attr("style", "background-color: green;");
+
+  if (currentHour > 15) {
+    $(".hour7").attr("style", "background-color: grey;");
+  } else if ((currentHour = 15)) {
+    $(".hour7").attr("style", "background-color: red;");
+  } else $(".hour7").attr("style", "background-color: green;");
+
+  if (currentHour > 16) {
+    $(".hour8").attr("style", "background-color: grey;");
+  } else if ((currentHour = 16)) {
+    console.log("XXX");
+    $(".hour8").attr("style", "background-color: red;");
+  } else {
+    $(".hour8").attr("style", "background-color: green;");
+  }
+
+  if (currentHour > 17) {
+    $(".hour9").attr("style", "background-color: grey;");
+  } else if ((currentHour = 17)) {
+    $(".hour9").attr("style", "background-color: red;");
+  } else $(".hour9").attr("style", "background-color: green;");
 });
-
-// Color-code time block: past (grey), current (red), future (green)
-// console.log(currentTime);
-
-// var currentTime = moment().format("LT");
-// var pastTime = currentTime.format(h);
-// var presentTime = moment().format;
-// var futureTime = moment().format("LT") > currentTime;
 
 // function getPastTime()
 // Allow user's input to persist, even after refresh
