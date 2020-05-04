d01da11a6cf2776a52bcdbe8d5fa231446e6f339
Tracy Vy <tracyvy88@gmail.com>
2020-05-04 12:32:27 -0700

    removed for loop

--- Develop/app.js
+++ Develop/app.js
@@ -2,8 +2,8 @@
 var m = moment();
 var currentHour = m.format("HH");
 var currentDate = m.format("MMMM Do, YYYY");
-const FIRST_HOUR = 9;
-const LAST_HOUR = 17;
+const firstHour = 9;
+const lastHour = 17;
 var tasks = {};
 console.log(currentHour);
 $(".dateLabel").html(currentDate);
@@ -24,31 +24,7 @@ $(document).ready(function () {
     $("#quoteOTD").html(`<p>${output.quote}  ${output.author}</p>`);
   });
 
-  tasks = JSON.parse(localStorage.getItem("tasks")) || {};
-  console.log("tasks from localStorage=" + JSON.stringify(tasks));
-
-  // Loop through and create task list
-  var myTaskList = $("#myTaskList");
-  var htmlTasks = "";
-  for (i = FIRST_HOUR; i < LAST_HOUR; i++) {
-    htmlTask +=
-      '<tr class="hour' + i + ">' + 
-      '<th scope="row" id="hourKey">9:00 AM</th>' + 
-      '<td><input class="inputEl" id="hour1" type="text" /></td>' + 
-      `<td>
-        <input
-          class="saveBtn"
-          type="button"
-          id="saveBtn1"
-          value="SAVE"
-          onclick="save("hour' + i + ")"
-        />
-      </td>` +
-    `</tr>`
-  }
-
   // Set color-coded functions: past (grey), current (red), future (green)
-  /**  
   if (currentHour > 9) {
     $(".hour1").attr("style", "background-color: grey;");
   } else if (currentHour == 9) {
@@ -109,7 +85,6 @@ $(document).ready(function () {
   if (currentHour > 16) {
     $(".hour8").attr("style", "background-color: grey;");
   } else if (currentHour == 16) {
-    console.log("XXX");
     $(".hour8").attr("style", "background-color: rgb(200, 35, 20)");
   } else {
     $(".hour8").attr("style", "background-color: #77dd77;");
@@ -122,20 +97,6 @@ $(document).ready(function () {
   } else {
     $(".hour9").attr("style", "background-color: #77dd77;");
   }
-  */
-
-  // Allow user's input to persist, even after refresh
-  var savedText = document.getElementById(".inputEl");
-  /**  
-  function displayToDo(){
-    console.log("Enter To Do");
-    inputEl.addEventListener("save", function(e){
-      e.preventDefault;
-      userInput = inputEl.value;
-      localStorage.setItem("savedText", savedText.value);
-    }
-  }
-  */
 });
 
 function save(hourId) {
